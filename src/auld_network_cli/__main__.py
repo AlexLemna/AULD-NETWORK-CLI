b75fa84d3425b3ab6164119ec72ef80272f286c4
Alex Lemna <git@alexanderlemna.com>
2025-10-18 22:15:00 -0500

    // wip

---
+++ src/auld_network_cli/__main__.py
@@ -0,0 +1,12 @@
+"""Entry point for running the Auld Network CLI as a module.
+
+This allows the package to be run with:
+    python -m auld_network_cli
+"""
+
+from __future__ import annotations
+
+from auld_network_cli import main
+
+if __name__ == "__main__":
+    main()
